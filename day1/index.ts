b4d63c5ba643d17ad47b7c559444aae4300ca0b0
fcaramez <francisco.caramez.duarte@gmail.com>
2023-12-05 09:10:42 +0000

    middle of part2 day1

--- day1/index.ts
+++ day1/index.ts
@@ -8,30 +8,55 @@ const isInt = (s: string): boolean => {
   return Number.isInteger(+s);
 };
 
+const SPELLED_NUM_MAP: Record<string, string> = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+const replaceSpelledNums = (s: string): string => {
+  let result = s;
+
+  for (const num in SPELLED_NUM_MAP) {
+    if (SPELLED_NUM_MAP.hasOwnProperty(num)) {
+      const numericEquivalent = SPELLED_NUM_MAP[num];
+      const regex = new RegExp(num, "gi");
+      result = result.replace(regex, numericEquivalent);
+    }
+  }
+
+  return result;
+};
+
 const main = async () => {
   const lines = await getInput();
 
   const numsToSum = [];
 
-  for (const s of lines) {
-    const currentNumber = [];
+  const linesWithoutSpelled = lines.map(s => {
+    return replaceSpelledNums(s);
+  });
+
+  for (const s of linesWithoutSpelled) {
+    const currentNumber: string[] = [];
+    const currentSpelledNums: string[] = [];
+    const numsInString: string[] = [];
 
     for (let i = 0; i < s.length; i++) {
       if (isInt(s[i])) {
-        currentNumber.push(s[i]);
-        break;
+        numsInString.push(s[i]);
       }
     }
 
-    for (let i = s.length - 1; i >= 0; i--) {
-      if (isInt(s[i])) {
-        currentNumber.push(s[i]);
-        break;
-      }
-    }
-    console.log(currentNumber);
-
-    const concatenatedNum = Number(`${currentNumber[0]}${currentNumber[1]}`);
+    const concatenatedNum = Number(
+      `${numsInString[0]}${numsInString[numsInString.length - 1]}`
+    );
     numsToSum.push(concatenatedNum);
   }
 
